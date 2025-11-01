f6d62523deee6a09dabaa90962a685886e79b74a
lop19011 <lop19011@byui.edu>
2025-10-31 20:02:58 -0600

    Added a random number gen game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("A random number has been generated between 1 - 100, can you guess it?");
+        Random randomNumber = new Random();
+        int magic = randomNumber.Next(1, 101);
+
+        Console.Write("what is your guess? ");
+        string magicGuess = Console.ReadLine();
+        int guess = int.Parse(magicGuess);
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            string newGuess = Console.ReadLine();
+            guess = int.Parse(newGuess);
+
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+
+           
+
+        }
+        if (guess == magic)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+
     }
 }
