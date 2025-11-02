920f93ca79e7be1d6e184d6b7c8f1e096c2152f8
lop19011 <lop19011@byui.edu>
2025-11-01 18:06:30 -0600

    Add a list and sum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        List<int> numbers = new List<int>();
+
+
+        int input = -1;
+
+        while (input != 0)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            input = int.Parse(userInput);
+            numbers.Add(input);
+        }
+
+        int totalSum = 0;
+
+        foreach (int sum in numbers)
+        {
+            totalSum += sum;
+        }
+
+         Console.WriteLine($"The total sum of your numbers is: {totalSum}");
+
+
+
+
+
+
+
     }
 }
