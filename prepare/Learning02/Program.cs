37ea21f094a982b4d643e763d557e8340fe12996
lop19011 <lop19011@byui.edu>
2025-11-11 18:06:20 -0700

    Added both new classes and tested a few lines

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Amazon";
+        job1._jobTitle = "IT";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+
+        Job job2 = new Job();
+        job2._company = "Steam";
+        job2._jobTitle = "IT help desk";
+        job2._startYear = 2015;
+        job2._endYear = 2020;
+
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Elias Jhonson";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+        
+
+
     }
 }
+
